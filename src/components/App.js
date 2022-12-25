ac79be29076865ea4291292e5e1f1ec8f72542e2
Ankit Kumar <akrjee15@gmail.com>
2022-12-26 00:13:43 +0530

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,16 @@ import '../styles/App.css';
 const App = () => {
 //code here 
 const [color, setColor] = useState("redColor")
+const [count, setCount] = useState(0)
+
 
 const handleColor =()=> {
-  setColor("blueColor");
+  setCount(count + 1)
+  if(count %2 ==0 ){
+    setColor("blueColor");
+  }else{
+    setColor("redColor");
+  }
 }
   return (
     <div id="main">
