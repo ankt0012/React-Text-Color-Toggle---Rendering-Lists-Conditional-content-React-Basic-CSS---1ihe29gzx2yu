2a03f6dfc18f2fb9a75d55bbf905b32dcf279b40
Ankit Kumar <akrjee15@gmail.com>
2022-12-26 23:39:48 +0530

    ....

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,18 @@ import '../styles/App.css';
 
 
 const App = () => {
-//code here 
-const [color, setColor] = useState("redColor")
-const [flag, setFlag] = useState(false)
+
+const [flag, setFlag] = useState(true)
 
 
 const handleColor =()=> {
   setFlag(!flag)
-  if (flag === true) {
-    setColor("blueColor");
-  }else{
-    setColor("redColor");
-  }
+
 }
   return (
     <div id="main">
-      <p className={color} >Newton School</p>
-      <button id='button' onClick={handleColor}>Change Style</button>
+      <p className={flag? "redColor" : "blueColor" } >Newton School</p>
+      <button id="button" onClick={handleColor}>Change Style</button>
     </div>
   )
 }
